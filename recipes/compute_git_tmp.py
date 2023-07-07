ab8bcca2ab56771499f3318c83f6b4335f8a59f6
admin <dlabouesse@live.fr>
2023-07-07 16:33:43 +0200

    Saved recipe: GITCONFLICT.compute_git_tmp

---
+++ recipes/compute_git_tmp.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+import dataiku
+import pandas as pd, numpy as np
+from dataiku import pandasutils as pdu
+
+
+
+# Compute recipe outputs
+# TODO: Write here your actual code that computes the outputs
+# NB: DSS supports several kinds of APIs for reading and writing data. Please see doc.
+
+git_tmp_df = ... # Compute a Pandas dataframe to write into git_tmp
+
+
+# Write recipe outputs
+git_tmp = dataiku.Dataset("git_tmp")
+git_tmp.write_with_schema(git_tmp_df)
